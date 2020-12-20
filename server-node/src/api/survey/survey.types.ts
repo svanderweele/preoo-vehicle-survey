e3ba8f59eeac999595df6fb42f35bcaa566d3807
Simon van der Weele <vanderweelesimon@gmail.com>
2020-12-20 12:36:58 +0100

    Added survey data

--- server-node/src/api/survey/survey.types.ts
+++ server-node/src/api/survey/survey.types.ts
@@ -1,6 +1,17 @@
+export interface SurveyDataVehicle01Data {
+  make: string;
+  model: string;
+}
+
 export interface SurveyDataVehicle01 {
   age: number;
   gender: 'Male' | 'Female' | 'Other';
+  doesOwnDrivingLicense: boolean;
+  isFirstCar: boolean;
+  driveTrainPreference: 'FWD' | 'RWD' | 'Unsure';
+  isWorriedAboutFuelEmission: boolean;
+  carCountInFamily: number;
+  carData: SurveyDataVehicle01Data[];
 }
 
 export type SurveyIds = 'survey_vehicle_01';
