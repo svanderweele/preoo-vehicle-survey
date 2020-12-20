c746362b37d65c28df9b38dafb10f4beb442733e
Simon van der Weele <vanderweelesimon@gmail.com>
2020-12-20 21:35:37 +0100

    Enabled CORS on api

--- server-node/src/main.ts
+++ server-node/src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 //TODO: Logging of exceptions
 //TODO: Rename Exception classes
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
