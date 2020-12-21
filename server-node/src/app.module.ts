9dbd86fb042cea5578116d9e267ba36f23d49ea1
Simon van der Weele <vanderweelesimon@gmail.com>
2020-12-21 12:27:20 +0100

    Added env file to backend

--- server-node/src/app.module.ts
+++ server-node/src/app.module.ts
@@ -3,9 +3,16 @@ import { DatabaseModule } from './api/database/database.module';
 import { SurveyModule } from './api/survey/survey.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [SurveyModule, DatabaseModule],
+  imports: [
+    SurveyModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development', '.env.development'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
